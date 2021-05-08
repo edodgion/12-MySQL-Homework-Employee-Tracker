21f3e68e063967c53f27dd60fc6bb1a74cdfe02b
Eric Dodgion <Barry.manowar@gmail.com>
2021-05-07 23:46:31 -0600

    2

--- Employee-Schema.sql
+++ Employee-Schema.sql
@@ -1,4 +1,34 @@
+DROP DATABASE IF EXISTS employee_db;
 
+CREATE DATABASE employee_db;
+
+USE emplyee_db;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NULL,
+     PRIMARY KEY(id)
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NULL,
+    salary DECIMAL NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES department (id) ON DELETE CASCADE,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NULL,
+    last_name varchar(30) NULL,
+    role_id INT NOT NULL,
+    FOREIGN KEY (role_id) REFERENCES role (id) ON DELETE CASCADE,
+    manager_id INT NULL,
+    FOREIGN KEY (manager_id) REFERENCES employee (id) ON DELETE SET NULL,
+    PRIMARY KEY (id)
+);
 
 
 
